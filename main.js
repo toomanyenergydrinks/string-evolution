014aad6838e21174198ff44d6c330e4e1ff9141d
Rehan Jaffer <ray@thelondonvandal.com>
2018-02-16 14:18:34 +0000

    moved asynchronous code into its own webworker, speeds up rendering, main file should only contain the update/display code

--- main.js
+++ main.js
@@ -1,131 +1,14 @@
-const target_str = "this string was created by evolving random strings!";
-
-function random_string(len) {
-
-  str = "";
-  r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  for(x=0;x<len;x++) {
-    str += r.charAt(Math.floor(Math.random()*r.length));
-  }
-  return str;
-
-}
-
-function create_generation(size) {
-
-  let candidates = [];
-  for (let x=0;x<size;x++) {
-    candidates[x] = random_string(Math.random()*50);
-  }
-  return candidates;
-
-}
-
-function fitness(str) {
-
-  let smallest = Math.min.apply(null,[str.length, target_str.length]);
-  let score = 0;
-  score += Math.abs(str.length - target_str.length) * 100;
-
-  for(let x=0;x<smallest;x++) {
-    score += Math.abs(str.charCodeAt(x) - target_str.charCodeAt(x));
-  }
-
-  return score;
-
-}
-
-function mutate(str) {
-
-  return str.split("").map(function(letter) {
-    if (Math.round(Math.random()*10) == 2) {
-      return String.fromCharCode(letter.charCodeAt(0)+Math.round(((Math.random()*2)-1)));
-    } else {
-      return letter;
-    }
-  }).join("");
-
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-function breed(str1, str2) {
-
-  cp_1 = getRandomInt(0, str1.length-1);
-  cp_2 = getRandomInt(cp_1, str1.length-1);
-
-  cp_3 = getRandomInt(0, str2.length-1);
-  cp_4 = getRandomInt(cp_3, str2.length-1);
-
-  s = (str1.slice(0, cp_1) + str2.slice(cp_3, cp_4) + str1.slice(cp_2, str2.length)).toString();
-  return s;
-
-}
-
-function update_page() {
-  
-}
-
-
 $(document).ready(function() {
 
-  var counter = 0;
-  const initial_generation_size = 200;
-  const generations = 1000;
-  const breeding_number = 20;
-  const allowed_survivors = 20;
-  const generation_skip = 10;
+  var worker = new Worker("worker.js");
+  var generation_counter = 0;
 
-  var generation_history = [];
-  var g = create_generation(initial_generation_size);
-  var new_g = [];
-
-  var int_f = setInterval(function() {
-    $("#display").html("<div id='string_entry'>" + generation_history[counter] + "</div>");
-    counter++;
-    if (counter >= (generations/generation_skip)) {
-      clearInterval(int_f);
-    }
-  }, 100);
-
-  var running = 1;
-
-  var x = 0;
-
-  for(let x = 0;x<generations;x++) {
-  while(running == 1) {
- 
-    new_g = g.map(function(c) { return [c, fitness(c)]; }).sort((a,b) => a[1] - b[1]);
-
-    var top = new_g.map((c) => c[0]).slice(0,allowed_survivors);
-
-    if (top[0][1] == 0) {
-      running = 0;
-    }
-
-    var top_kids = []
-
-    for(let y=0;y<breeding_number;y++) {
-      for(let i=0;i<(breeding_number-y);i++) {
-        top_kids.push(breed(top[y],top[getRandomInt(0,i)]));
-      }
-    }
-
-    g = top
-
-    g = g.concat(create_generation(100)).concat(top_kids).concat(top).map((x) => mutate(x));
-
-    if (x % generation_skip == 0) {
-      generation_history = generation_history.concat(new_g[0][0]);
-    }
-
-    x = x + 1;
-
-  }
+  worker.onmessage = function(e) {
+    generation_counter += 1;
+    $("#display").append("<div id='string_entry'> G# " + generation_counter + ": " + e.data + "</div>");
+    var realHeight = $("#display")[0].scrollHeight;
+    $("#display").scrollTop(realHeight);
+  };
 
 
 });
